efa9fe8a05d11043aead206d905f3c57d487aa07
Meltasy <119603631+Meltasy@users.noreply.github.com>
2024-05-23 12:27:24 +0200

    Add functions to the simple calculator
    Update the current functions to improve usability
    Add new functions for clear screen and clear all
    Make small changes to user interface in css

--- script.js
+++ script.js
@@ -1,29 +1,83 @@
-const num1 = 0
-const num2 = 0
-const operator = 0
+// Pseudocode
 
-function add(num1, num2) {
-    return num1 + num2;
+// #1 Initialize the state of the application: current number = 0; current result = 0
+// #2 Create the current number with the operand buttons pressed by user
+// #3 When operator button is pressed, update current result with current number
+// #4 Store the value of the operator
+// #5 Repeat #2
+// #6 When equals button is pressed, calculate using stored information and update current result
+
+let currentNumber = 0;
+let currentResult = 0;
+let currentOperator = "";
+
+const allOperands = document.querySelectorAll("#operand");
+const allOperators = document.querySelectorAll("#operator");
+
+let displayDiv = document.getElementById("display");
+
+function showInDisplay(number) {
+    displayDiv.textContent = number;
 }
 
-function subtract(num1, num2) {
-    return num1 - num2;
+allOperands.forEach(operand => {
+    operand.addEventListener("click", (e) => {
+        currentNumber = 10 * currentNumber + parseInt(e.target.value);
+        console.log(currentNumber);
+        showInDisplay(currentNumber);
+    });
+});
+
+allOperators.forEach(operator => {
+    operator.addEventListener("click", (e) => {
+        console.log(e.target.value);
+        currentOperator = e.target.value;
+        currentResult = currentNumber;
+        currentNumber = 0;
+    });
+});
+
+function operate(num1, num2, operator) {
+    switch(operator) {
+        case "+":
+            this.number = num1 + num2;
+            break;
+        case "-":
+            this.number = num1 - num2;
+            break;
+        case "*":
+            this.number = num1 * num2;
+            break;
+        case "/":
+            this.number = num1 / num2;
+            break;
+    }
+    console.log(this.number);
+    return this.number;
 }
 
-function multiply(num1, num2) {
-    return num1 * num2;
-}
+let equalBtn = document.getElementById("equals");
 
-function divide(num1, num2) {
-    return num1 / num2;
-}
+equalBtn.addEventListener("click", (e) => {
+    currentResult = operate(currentResult, currentNumber, currentOperator);
+    console.log(currentResult);
+    showInDisplay(currentResult);
+    currentNumber = currentResult;
+});
 
-// console.log(add(37, 29));
-// console.log(subtract(88, 41));
-// console.log(multiply(6, 8));
-// console.log(divide(56, 7));
+let clearAllBtn = document.getElementById("clearAll");
 
-function Operate(operator, num1, num2) {
-    operator = add || subtract || multiply || divide;
-    return num1.operator + num2.operator;
-}
+clearAllBtn.addEventListener("click", (e) => {
+    currentNumber = 0;
+    currentResult = 0;
+    currentOperator = "";
+    console.log(currentResult);
+    showInDisplay(currentResult);
+});
+
+let clearEntryBtn = document.getElementById("clearEntry");
+
+clearEntryBtn.addEventListener("click", (e) => {
+    currentNumber = 0;
+    showInDisplay();
+});
